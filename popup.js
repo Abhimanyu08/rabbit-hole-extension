95aee46ad0d6c37edeafaa7faa695e485afbfdcb
Abhimanyu. <iamabhimanyu08@gmail.com>
2023-09-08 17:03:39 +0530

    urls along side nodes

--- popup.js
+++ popup.js
@@ -1,9 +1,12 @@
 const nodeToCoords = new Map();
 
 const scale = d3.scaleLinear([0, 1], [100, 600]);
+const transformScale = [1,10]
+const opacityScale = d3.scaleLinear(transformScale, [0,1])
 const line = d3.line();
 const circles =[]
  const paths = []
+ const urls = []
 
 document.addEventListener("DOMContentLoaded", async () => {
     let data = await new Promise((resolve, reject) => {
@@ -21,14 +24,17 @@ document.addEventListener("DOMContentLoaded", async () => {
 	
 	renderNode(urlNode, undefined, svg)
 
-	const circleElems = svg.selectAll("circle").data(circles).join("circle").attr("transform", d => `translate(${d})`).attr("r", 5).attr("fill", "red")
-  	const pathElems = svg.selectAll("path").data(paths).join("path").attr("d", (d) => line(d)).attr("strokeWidth", 3).attr("stroke", "white")
+	const circleElems = svg.selectAll("circle").data(circles).join("circle").attr("transform", d => `translate(${d})`).attr("r", 5).attr("fill", "cornflowerblue")
+  	const pathElems = svg.selectAll("path").data(paths).join("path").attr("d", (d) => line(d)).attr("strokeWidth", 3).attr("stroke", "turquoise")
+	const labels = svg.selectAll("text").data(circles).join("text")
+	.attr("transform", d => `translate(${d})`).attr("fill", "white").text((_,i) => urls[i]).style("opacity", 0)
 	function zoomed({transform}) {
-	circleElems.attr("r", 5*transform.k).attr("transform", (d) => `translate(${transform.apply(d)})`)
-	pathElems.attr("transform", transform)
+		circleElems.attr("r", 5*transform.k).attr("transform", (d) => `translate(${transform.apply(d)})`)
+		pathElems.attr("transform", transform)
+		labels.attr("transform", (d) => `translate(${transform.apply(d)})`).style("opacity", opacityScale(transform.k))
 	}
 
- 	svg.call(d3.zoom().on("zoom", zoomed))
+ 	svg.call(d3.zoom().scaleExtent(transformScale).on("zoom", zoomed))
 
 
 });
@@ -45,29 +51,8 @@ function renderNode(node, parentCoords, container) {
 	const coords = nodeToCoords.get(node.url);
 
 	circles.push([coords.x, coords.y])
-	// container.append("circle")
-	// 	.attr("cx", coords.x)
-	// 	.attr("cy", coords.y)
-	// 	.attr("r", 5)
-	// 	.attr("fill", "cornflowerblue")
-
-		// container.append("text")
-		// .attr("x", coords.x)
-		// .attr("y", coords.y + 22)
-		// .attr("fill","white")
-		// .text(node.url);
+	urls.push(node.url)
 	if (parentCoords) {
-		// container.append("path")
-		// 	.attr("fill", "none")
-		// 	.attr("strokeWidth", 3)
-		// 	.attr("stroke", "turquoise")
-		// 	.attr(
-		// 		"d",
-		// 		line([
-		// 			[coords.x, coords.y],
-		// 			[parentCoords.x, parentCoords.y],
-		// 		])
-		// 	);
 			paths.push([[coords.x,coords.y], [parentCoords.x, parentCoords.y]])
 	}
 
