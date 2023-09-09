61bea789f60d1ba072548e2b0451da966b5d6e9f
Abhimanyu. <iamabhimanyu08@gmail.com>
2023-09-09 15:56:34 +0530

    nodes draggable

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
 const nodeToCoords = new Map();
 
 const scale = d3.scaleLinear([0, 1], [100, 600]);
-const transformScale = [1, 10];
-const opacityScale = d3.scaleLinear(transformScale, [0, 1]);
+const transformScale = [1, 3];
+const opacityScale = d3.scaleLinear(transformScale, [0, 0.5]);
 const line = d3.line();
 const circles = [];
 const paths = [];
@@ -23,42 +23,33 @@ document.addEventListener("DOMContentLoaded", async () => {
 	const urlNode = JSON.parse(data.urlNode);
 
 	renderNode(urlNode, 0, undefined, svg);
-	console.log(paths, circles);
 
-	const pathElems = svg
-		.selectAll("line")
-		.data(paths)
-		.join("line")
-		.attr("stroke", "turquoise")
-		.attr("stroke-width", 2);
+	const pathElems = preparePaths(svg);
+	const circleElems = prepareNodes(svg);
+	const labels = prepareLabels(svg);
 
-	const labels = svg
-		.selectAll("text")
-		.data(circles)
-		.join("text")
-		// .attr("transform", (d) => `translate(${d})`)
-		.attr("fill", "white")
-		.text((_, i) => urls[i])
-		.style("opacity", 0);
-	const circleElems = svg
-		.selectAll("circle")
-		.data(circles)
-		.join("circle")
-		// .attr("transform", (d) => `translate(${d})`)
-		.attr("r", 5)
-		.attr("fill", "cornflowerblue")
-		.on("mouseover", function (_, d) {
-			console.log(d);
-			const label = labels.filter((ld, i) => d.url === ld.url);
-
-			label.attr("opacityOld", label.style("opacity"));
-			label.style("opacity", 1);
+	circleElems
+		.on("click", function (_, d) {
+			const url = d.url;
+			chrome.tabs.create({ url });
 		})
+		.on("mouseover", function (_, d) {
+			const label = labels.filter((ld, _) => d.url === ld.url);
+			label.attr("opacityOld", label.style("opacity"));
+			labels.style("opacity", 0);
+			label.style("opacity", 1);
+
+			circleElems.style("opacity", 0.2);
+			pathElems.style("opacity", 0.2);
+		})
+
 		.on("mouseleave", function (_, d) {
-			console.log(d);
-			const label = labels.filter((ld, i) => d.url === ld.url);
-			label.style("opacity", label.attr("opacityOld"));
+			const label = labels.filter((ld, _) => d.url === ld.url);
+			labels.style("opacity", label.attr("opacityOld"));
+			circleElems.style("opacity", 1);
+			pathElems.style("opacity", 1);
 		});
+
 	function zoomed({ transform }) {
 		circleElems.attr("transform", transform);
 		pathElems.attr("transform", transform);
@@ -69,10 +60,11 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 	const simulation = d3
 		.forceSimulation(circles)
+		.force("link", d3.forceLink(paths))
 		.force("x", d3.forceX(200))
 		.force("y", d3.forceY(200))
-		.force("collide", d3.forceCollide(10))
-		.force("link", d3.forceLink(paths));
+		.force("collide", d3.forceCollide(20))
+		.force("center", d3.forceCenter(700 / 2, 500 / 2));
 
 	svg.call(d3.zoom().scaleExtent(transformScale).on("zoom", zoomed));
 
@@ -84,9 +76,35 @@ document.addEventListener("DOMContentLoaded", async () => {
 			.attr("y2", (d) => d.target.y);
 
 		circleElems.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
-		labels.attr("x", (d) => d.x).attr("y", (d) => d.y);
+		labels.attr("x", (d) => d.x).attr("y", (d) => d.y + 10);
 		// pathElems.attr("d", (d) => line(d))
 	});
+	circleElems.call(
+		d3
+			.drag()
+			.on("start", dragstarted)
+			.on("drag", dragged)
+			.on("end", dragended)
+	);
+	function dragstarted(event) {
+		if (!event.active) simulation.alphaTarget(0.3).restart();
+		event.subject.fx = event.subject.x;
+		event.subject.fy = event.subject.y;
+	}
+
+	// Update the subject (dragged node) position during drag.
+	function dragged(event) {
+		event.subject.fx = event.x;
+		event.subject.fy = event.y;
+	}
+
+	// Restore the target alpha so the simulation cools after dragging ends.
+	// Unfix the subject position now that it’s no longer being dragged.
+	function dragended(event) {
+		if (!event.active) simulation.alphaTarget(0);
+		event.subject.fx = null;
+		event.subject.fy = null;
+	}
 });
 
 function renderNode(node, parentIndex, parentCoords, container) {
@@ -118,4 +136,39 @@ function renderNode(node, parentIndex, parentCoords, container) {
 			);
 		});
 	}
+}
+
+function preparePaths(svg) {
+	const pathElems = svg
+		.selectAll("line")
+		.data(paths)
+		.join("line")
+		.attr("stroke", "turquoise")
+		.attr("stroke-width", 2);
+	return pathElems;
+}
+
+function prepareLabels(svg) {
+	const labels = svg
+		.append("g")
+		.selectAll("text")
+		.data(circles)
+		.join("text")
+		.attr("fill", "white")
+		.text((_, i) => urls[i])
+		.style("opacity", 0)
+		.style("font-size", "5px")
+		.style("font-weight", "400");
+	return labels;
+}
+
+function prepareNodes(svg) {
+	const circleElems = svg
+		.selectAll("circle")
+		.data(circles)
+		.join("circle")
+		.attr("r", 5)
+		.attr("fill", "cornflowerblue");
+
+	return circleElems;
 }
