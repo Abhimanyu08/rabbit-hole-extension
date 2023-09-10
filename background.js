d4a176772e853d110cd8e23376010682ca1f57cf
Abhimanyu. <iamabhimanyu08@gmail.com>
2023-09-10 16:11:59 +0530

    prompt user and hijack history

--- background.js
+++ background.js
@@ -3,25 +3,23 @@ let urlToNodeMap = new Map();
 let urlNode = {
 	url: "root",
 	children: [],
-	parent: null
+	parent: null,
 };
 
 urlToNodeMap.set("root", urlNode);
 
+// chrome.runtime.onMessage.addListener((message) => {
+// 	console.log(message);
+// 	if (message === "start-digging") {
+// 		startDigging = true;
+// 		return;
+// 	}
+// 	startDigging = false;
+// });
+
 chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
 	//this url is child of the root
-	console.log(urlNode, currentUrl, details.url)
-	const rootNode = urlToNodeMap.get(currentUrl);
-	const currentNode = urlToNodeMap.has(details.url)
-		? urlToNodeMap.get(details.url)
-		: { url: details.url, children: []};
-	if (currentNode.url !== rootNode.parent && rootNode.url !== currentNode.url) {
-		currentNode.parent = rootNode.url
-		rootNode.children.push(currentNode);
-	}
-	currentUrl = details.url;
-	urlToNodeMap.set(currentUrl, currentNode);
-	chrome.storage.local.set({ urlNode: JSON.stringify(urlNode) })	
+	updateGraph(details.url);
 });
 
 // Listen for changes in the active tab.
@@ -29,12 +27,59 @@ chrome.tabs.onActivated.addListener(({ tabId }) => {
 	// Use the chrome.tabs.get method to retrieve information about the active tab.
 	chrome.tabs.get(tabId, (tab) => {
 		// Check if the tab's URL starts with "chrome://newtab/".
-		if (tab.url && tab.url.startsWith("chrome://newtab/")) {
-			// The user is on a Chrome New Tab page, reset everything
-			reset();
-		}
+		// if (tab.url && !tab.url.startsWith("chrome://newtab")) {
+		// 	currentUrl = tab.url;
+		// }
 	});
 });
+
+chrome.tabs.onCreated.addListener(function (tab) {
+	// Check if the tab has a "chrome://" URL (new tab page)
+	if (tab.url && tab.url.startsWith("chrome://")) {
+		// This tab is likely a new tab page
+		console.log("User opened a new tab page");
+	}
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+	// Check if the URL changes to a "chrome://" URL (new tab page)
+	if (changeInfo.url && changeInfo.url.startsWith("chrome://")) {
+		// This tab is likely a new tab page
+		// updateGraph(changeInfo.url);
+		return;
+	}
+
+	if (changeInfo.url) {
+		updateGraph(changeInfo.url);
+	}
+});
+
+function updateGraph(url) {
+	chrome.storage.session.get("startDigging", function (data) {
+		if (!data.startDigging) return;
+		console.log(urlNode);
+		const rootNode = urlToNodeMap.has(currentUrl)
+			? urlToNodeMap.get(currentUrl)
+			: {
+					url,
+					children: [],
+					parent: null,
+			  };
+		const currentNode = urlToNodeMap.has(url)
+			? urlToNodeMap.get(url)
+			: { url, children: [] };
+		if (
+			currentNode.url !== rootNode.parent &&
+			rootNode.url !== currentNode.url
+		) {
+			currentNode.parent = rootNode.url;
+			rootNode.children.push(currentNode);
+		}
+		currentUrl = url;
+		urlToNodeMap.set(currentUrl, currentNode);
+		chrome.storage.local.set({ urlNode: JSON.stringify(urlNode) });
+	});
+}
 
 function reset() {
 	currentUrl = "root";
@@ -42,7 +87,7 @@ function reset() {
 	urlNode = {
 		url: "root",
 		children: [],
-		parent:null
+		parent: null,
 	};
 	urlToNodeMap.set("root", urlNode);
 	chrome.storage.local.clear();
--- popup.js
+++ popup.js
@@ -1,174 +1,24 @@
-const nodeToCoords = new Map();
+document.addEventListener("DOMContentLoaded", () => {
+	const inputEl = document.getElementById("start");
+	const label = document.getElementsByTagName("label").item(0);
 
-const scale = d3.scaleLinear([0, 1], [100, 600]);
-const transformScale = [1, 3];
-const opacityScale = d3.scaleLinear(transformScale, [0, 0.5]);
-const line = d3.line();
-const circles = [];
-const paths = [];
-const urls = [];
-
-document.addEventListener("DOMContentLoaded", async () => {
-	let data = await new Promise((resolve, reject) => {
-		chrome.storage.local.get("urlNode", (result) => {
-			if (chrome.runtime.lastError) {
-				reject(chrome.runtime.lastError);
-			} else {
-				resolve(result);
-			}
-		});
+	chrome.storage.session.get("startDigging", function (data) {
+		inputEl.checked = data.startDigging || false;
+		if (data.startDigging) {
+			label.textContent = "Stop Digging";
+		}
 	});
-	const svg = d3.select("#svg");
 
-	const urlNode = JSON.parse(data.urlNode);
+	inputEl.addEventListener("change", (e) => {
+		if (e.target.checked) {
+			// chrome.runtime.sendMessage("start-digging");
+			chrome.storage.session.set({ startDigging: true });
+			label.textContent = "Stop Digging";
+			return;
+		}
 
-	renderNode(urlNode, 0, undefined, svg);
-
-	const pathElems = preparePaths(svg);
-	const circleElems = prepareNodes(svg);
-	const labels = prepareLabels(svg);
-
-	circleElems
-		.on("click", function (_, d) {
-			const url = d.url;
-			chrome.tabs.create({ url });
-		})
-		.on("mouseover", function (_, d) {
-			const label = labels.filter((ld, _) => d.url === ld.url);
-			label.attr("opacityOld", label.style("opacity"));
-			labels.style("opacity", 0);
-			label.style("opacity", 1);
-
-			circleElems.style("opacity", 0.2);
-			pathElems.style("opacity", 0.2);
-		})
-
-		.on("mouseleave", function (_, d) {
-			const label = labels.filter((ld, _) => d.url === ld.url);
-			labels.style("opacity", label.attr("opacityOld"));
-			circleElems.style("opacity", 1);
-			pathElems.style("opacity", 1);
-		});
-
-	function zoomed({ transform }) {
-		circleElems.attr("transform", transform);
-		pathElems.attr("transform", transform);
-		labels
-			.attr("transform", transform)
-			.style("opacity", opacityScale(transform.k));
-	}
-
-	const simulation = d3
-		.forceSimulation(circles)
-		.force("link", d3.forceLink(paths))
-		.force("x", d3.forceX(200))
-		.force("y", d3.forceY(200))
-		.force("collide", d3.forceCollide(20))
-		.force("center", d3.forceCenter(700 / 2, 500 / 2));
-
-	svg.call(d3.zoom().scaleExtent(transformScale).on("zoom", zoomed));
-
-	simulation.on("tick", () => {
-		pathElems
-			.attr("x1", (d) => d.source.x)
-			.attr("y1", (d) => d.source.y)
-			.attr("x2", (d) => d.target.x)
-			.attr("y2", (d) => d.target.y);
-
-		circleElems.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
-		labels.attr("x", (d) => d.x).attr("y", (d) => d.y + 10);
-		// pathElems.attr("d", (d) => line(d))
+		// chrome.runtime.sendMessage("stop-digging");
+		chrome.storage.session.set({ startDigging: false });
+		label.textContent = "Start Digging";
 	});
-	circleElems.call(
-		d3
-			.drag()
-			.on("start", dragstarted)
-			.on("drag", dragged)
-			.on("end", dragended)
-	);
-	function dragstarted(event) {
-		if (!event.active) simulation.alphaTarget(0.3).restart();
-		event.subject.fx = event.subject.x;
-		event.subject.fy = event.subject.y;
-	}
-
-	// Update the subject (dragged node) position during drag.
-	function dragged(event) {
-		event.subject.fx = event.x;
-		event.subject.fy = event.y;
-	}
-
-	// Restore the target alpha so the simulation cools after dragging ends.
-	// Unfix the subject position now that it’s no longer being dragged.
-	function dragended(event) {
-		if (!event.active) simulation.alphaTarget(0);
-		event.subject.fx = null;
-		event.subject.fy = null;
-	}
 });
-
-function renderNode(node, parentIndex, parentCoords, container) {
-	const xCoord = Math.floor(scale(Math.random()));
-	const yCoord = parentCoords ? parentCoords.y + 20 : 20;
-
-	if (!nodeToCoords.has(node.url)) {
-		nodeToCoords.set(node.url, { x: xCoord, y: yCoord });
-	}
-	const coords = nodeToCoords.get(node.url);
-
-	circles.push({ ...coords, url: node.url });
-	urls.push(node.url);
-	if (parentCoords) {
-		paths.push({
-			source: parentIndex,
-			target: circles.length - 1,
-			url: node.url,
-		});
-	}
-
-	if (node.children) {
-		node.children.map((child) => {
-			renderNode(
-				child,
-				circles.length - 1,
-				{ x: coords.x, y: coords.y },
-				container
-			);
-		});
-	}
-}
-
-function preparePaths(svg) {
-	const pathElems = svg
-		.selectAll("line")
-		.data(paths)
-		.join("line")
-		.attr("stroke", "turquoise")
-		.attr("stroke-width", 2);
-	return pathElems;
-}
-
-function prepareLabels(svg) {
-	const labels = svg
-		.append("g")
-		.selectAll("text")
-		.data(circles)
-		.join("text")
-		.attr("fill", "white")
-		.text((_, i) => urls[i])
-		.style("opacity", 0)
-		.style("font-size", "5px")
-		.style("font-weight", "400");
-	return labels;
-}
-
-function prepareNodes(svg) {
-	const circleElems = svg
-		.selectAll("circle")
-		.data(circles)
-		.join("circle")
-		.attr("r", 5)
-		.attr("fill", "cornflowerblue");
-
-	return circleElems;
-}
